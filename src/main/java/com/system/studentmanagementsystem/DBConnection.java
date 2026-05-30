ab92d0807660dafce697250db35328080521631d
miluhx <katrinamaecmila@gmail.com>
2026-05-30 15:23:25 +0800

    Refactor database connection to use secure config properties and update gitignore

--- src/main/java/com/system/studentmanagementsystem/DBConnection.java
+++ src/main/java/com/system/studentmanagementsystem/DBConnection.java
@@ -1,15 +1,27 @@
 package com.system.studentmanagementsystem;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public class DBConnection {
     public static Connection connect() {
         try {
+            Properties prop = new Properties();
+            InputStream input = DBConnection.class.getClassLoader().getResourceAsStream("config.properties");
+
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+                return null;
+            }
+
+            prop.load(input);
+
             return DriverManager.getConnection(
-                    "jdbc:postgresql://localhost:5432/studentdb",
-                    "postgres",
-                    "093233508"
+                    prop.getProperty("db.url"),
+                    prop.getProperty("db.user"),
+                    prop.getProperty("db.password")
             );
         } catch (Exception e) {
             e.printStackTrace();
